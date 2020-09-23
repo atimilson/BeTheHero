5110c65301f0be23b17a0ee2c67145bccd83dfe9
Whalyf Viana <whalyf_smv@hotmail.com>
2020-09-23 14:02:26 -0300

    Add files via upload

---
+++ backend/knexfile.js
@@ -0,0 +1,61 @@
+// Update with your config settings.
+//Configurações de acesso ao banco de dados para cada cenário de uso
+module.exports = {
+
+  //Ambiente de desenvolvimento
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
+ //Ambiente que simula o ambiente online para o time de desenvolvimento 
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+ //Ambiente online para clientes acessarem
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
